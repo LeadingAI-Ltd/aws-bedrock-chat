b87b7ecdad873134dee7f8f0eec25237b6f2f902
ibbyleadingai <muhammad.ibrahim@leadingai.co.uk>
2025-06-18 11:08:20 +0100

    Revert "Update run.sh"
    This reverts commit b8459024f7a7ae1d02ecfb71cdc04291a81ac073.

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
-export PATH=$PATH:$LAMBDA_TASK_ROOT/bin
-export PYTHONPATH=$PYTHONPATH:/opt/python:$LAMBDA_RUNTIME_DIR
-exec python -m uvicorn --port=$PORT app.main:app
+#!/bin/bash
+
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin \
+    PYTHONPATH=$PYTHONPATH:/opt/python:$LAMBDA_RUNTIME_DIR \
+    exec python -m uvicorn --port=$PORT app.main:app
